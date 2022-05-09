a19b66d0c18ea040e694eb748308776c1d18061a
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-05-08 22:09:13 -0500

    change connection to previous

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 const { connect, connection } = require("mongoose");
 
-connect("mongodb://localhost:27017/socialnetworkDB", {
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/socialNetworkDB";
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
